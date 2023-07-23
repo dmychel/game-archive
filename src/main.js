d5e35345f57a0af3da0173a3cb053db4fd3009ad
dmychel <dmychel@tutanota.com>
2023-07-23 15:47:19 -0400

    objects and arrays
    -made an empty archive array
    -made a Game object constructor that takes the title of a game (for now)
    -made a function that takes a user prompt and creates a new object with the users input and pushes it into an array

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
-console.log('Hello World')
+let archive = [];
+let newGame;
+
+function Game(title){
+    this.title = title,
+    // this.completed = completed,
+    this.info = function(){
+        console.log(title)
+    }
+}
+
+function addGameToArchive(){
+    let game = prompt();
+    newGame = new Game(game);
+    archive.push(newGame)
+}
+
+addGameToArchive()
+console.log(archive)

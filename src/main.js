526d0816f9e08402fa3d85b28a367ebcb7314c24
dmychel <dmychel@tutanota.com>
2023-07-23 16:16:22 -0400

    loop
    -added for loop that loops through archive array and displays the title of the newGame object onto the gameArchive html section

--- src/main.js
+++ src/main.js
@@ -1,19 +1,29 @@
+const gameArchive = document.querySelector('#archive');
+
+
 let archive = [];
 let newGame;
 
 function Game(title){
-    this.title = title,
+    this.title = title
     // this.completed = completed,
-    this.info = function(){
-        console.log(title)
+}
+
+function displayArchive(){
+    for(let i = 0; i < archive.length; i++){
+        let gameTitle = archive[i].title;
+        gameArchive.append(gameTitle)
+        console.log(archive[i])
     }
 }
 
 function addGameToArchive(){
     let game = prompt();
     newGame = new Game(game);
-    archive.push(newGame)
+    archive.push(newGame);
+    displayArchive();
 }
 
+
+
 addGameToArchive()
-console.log(archive)

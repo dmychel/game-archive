fdaa2a8480b0bd5a24f843be43f012a5a186f16a
dmychel <dmychel@tutanota.com>
2023-07-27 16:43:16 -0400

    gamecard
    -displayarchive creates a div named game card with an h2 as the game title value

--- src/main.js
+++ src/main.js
@@ -1,28 +1,37 @@
 const gameArchive = document.querySelector('#archive');
 const form = document.querySelector('#form')
 
-
-
-
 let archive = [];
-let newGame;
+let game;
 
 function Game(title){
     this.title = title
     // this.completed = completed,
 }
 
+function addGameToLibrary(){
+    let title = document.querySelector('#gameTitle').value;
+    let newGame = new Game(title);
+    archive.push(newGame)
+    console.log(archive)
+}
+
 function displayArchive(){
+    gameArchive.innerHTML = '';
     for(let i = 0; i < archive.length; i++){
         let gameTitle = archive[i].title;
-        gameArchive.append(gameTitle)
-        console.log(archive[i])
+        let gameCard = document.createElement('div');
+        gameCard.innerHTML = `<h2>${gameTitle}<h2>`
+        gameCard.classList = 'game-card';
+        gameArchive.appendChild(gameCard)
     }
 }
 
 form.addEventListener('submit', (e)=> {
     e.preventDefault();
-    const gameTitle = document.querySelector('#gameTitle');
-    let game =  gameTitle.value;
-    console.log(game)
+
+    addGameToLibrary();
+    displayArchive();
 })
+
+

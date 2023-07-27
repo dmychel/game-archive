d965defb852a45bd3550d47f9e870850872d1cc7
dmychel <dmychel@tutanota.com>
2023-07-27 17:04:41 -0400

    Remove button
    -added remove button to game card that runs  remove function onclick
    -the remove function remvoes the game from the array, then the displayarchive function is run again to display the updated archive

--- src/main.js
+++ src/main.js
@@ -2,36 +2,45 @@ const gameArchive = document.querySelector('#archive');
 const form = document.querySelector('#form')
 
 let archive = [];
-let game;
+
 
 function Game(title){
     this.title = title
     // this.completed = completed,
-}
+};
 
 function addGameToLibrary(){
     let title = document.querySelector('#gameTitle').value;
     let newGame = new Game(title);
     archive.push(newGame)
     console.log(archive)
-}
+};
+
+function removeCard(game) {
+    archive.splice(game,1);
+    displayArchive()
+    console.log(archive)
+};
 
 function displayArchive(){
     gameArchive.innerHTML = '';
     for(let i = 0; i < archive.length; i++){
         let gameTitle = archive[i].title;
         let gameCard = document.createElement('div');
-        gameCard.innerHTML = `<h2>${gameTitle}<h2>`
+        gameCard.innerHTML = `
+        <h2>${gameTitle}</h2>
+        <button class="exit-btn" onclick="removeCard(${i})">&#10006;</button>
+        `
         gameCard.classList = 'game-card';
         gameArchive.appendChild(gameCard)
     }
-}
+};
 
 form.addEventListener('submit', (e)=> {
     e.preventDefault();
 
     addGameToLibrary();
     displayArchive();
-})
+});
 
 

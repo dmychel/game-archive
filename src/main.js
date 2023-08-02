3f0998159081828a6ee81de49127e881e6d3f8c1
dmychel <dmychel@tutanota.com>
2023-08-02 16:05:53 -0400

    Local Storage
    -when the user adds a game through the form the game is added to the archive array, then a local storage item is created from the archive array.
    
    Display:
    when the archive is displayed a variable is set equal to "JSON.parse(localStorage.getItem('archive'));" and then the archive array is set equal to that variable.
    
    Remove Game:
    To remove a game - first, the archive is updated, then the local storage is completely cleared. I then create another local storage item set to the new archive array

--- src/main.js
+++ src/main.js
@@ -10,90 +10,53 @@ const yk = document.createElement('img').src = 'img/game-covers/yakuza.jpg';
 const sonic = document.createElement('img').src = 'img/game-covers/sonic.jpg';
 const gaiden = document.createElement('img').src = 'img/game-covers/ninja-gaiden.jpg';
 
-const windwaker = { 
-    title: 'The Legend of Zela: The Wind Waker',
-    genre: 'Adventure/Puzzle',
-    hours: 150,
-    completed: 'completed',
-    image: ww
-}
 
-const dragon = {
-    title: 'Yakuza: Like a Dragon',
-    genre: 'JRPG',
-    hours: 70,
-    completed: 'not completed',
-    image: ykl
-}
-
-const yakuza = {
-    title: 'Yakuza Kiwami 2 (Sub)',
-    genre: 'Action/Adventure',
-    hours: 70,
-    completed: 'completed',
-    image: yk
-}
-
-const SA2 = {
-    title: 'Sonic Adventure 2 Battle',
-    genre: 'Action/Adventure',
-    hours: 1000,
-    completed: 'completed',
-    image: sonic
-}
-
-const ninja = {
-    title: 'Ninja Gaiden Black',
-    genre: 'Action/Hard...',
-    hours: 50,
-    completed: 'completed',
-    image: gaiden
-}
-
-let archive = [windwaker,dragon,SA2,ninja,yakuza];
+let archive = []
 
 
-function Game(title, genre, hours, completed, image){
+function Game(title, genre, hours, completed, image) {
     this.title = title,
-    this.genre = genre,
-    this.hours = hours,
-    this.completed = completed,
-    this.image = image
+        this.genre = genre,
+        this.hours = hours,
+        this.completed = completed,
+        this.image = image
 };
 
-function addGameToLibrary(){
+function addGameToLibrary() {
     // Game values
     let title = document.querySelector('#gameTitle').value;
     let genre = document.querySelector('#gameGenre').value;
     let hours = document.querySelector('#gameHours').value;
     let completed = document.querySelector('#gameCompleted').value;
-    console.log(completed)
 
     // image url
     let imageFile = document.querySelector('#gameImage');
     let image;
-    if(imageFile.files.length == 0){
+    if (imageFile.files.length == 0) {
         imageElement = document.createElement('img');
         image = imageElement.src = 'img/placeholder.jpg';
     }
     else {
-       image = imageFile.src = URL.createObjectURL(imageFile.files[0]);
+        image = imageFile.src = URL.createObjectURL(imageFile.files[0]);
     }
 
-    let newGame = new Game(title,genre,hours,completed,image);
+    let newGame = new Game(title, genre, hours, completed, image);
     archive.push(newGame)
-    console.log(archive)
+    localStorage.setItem('archive', JSON.stringify(archive));
 };
 
 function removeCard(game) {
-    archive.splice(game,1);
+    archive.splice(game, 1);
+    localStorage.clear();
+    localStorage.setItem('archive', JSON.stringify(archive));
     displayArchive()
-    console.log(archive)
 };
 
-function displayArchive(){
+function displayArchive() {
+    let userData = JSON.parse(localStorage.getItem('archive'));
+    archive = userData;
     gameArchive.innerHTML = '';
-    for(let i = 0; i < archive.length; i++){
+    for (let i = 0; i < archive.length; i++) {
         let gameTitle = archive[i].title;
         let gameGenre = archive[i].genre;
         let gameHours = archive[i].hours;
@@ -120,13 +83,13 @@ function displayArchive(){
         `
         gameCard.classList = 'game-card';
         gameArchive.appendChild(gameCard);
-        
+
         // set users completed select
         const selected = document.querySelector('#selected').value = `${gameCompleted}`;
     }
 };
 
-form.addEventListener('submit', (e)=> {
+form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     addGameToLibrary();
@@ -140,12 +103,63 @@ form.addEventListener('submit', (e)=> {
 });
 
 
-function displayForm(){
+function displayForm() {
     formContainer.style.display = 'block';
 }
 
-function removeForm(){
+function removeForm() {
     formContainer.style.display = 'none'
 }
 
-displayArchive()
+function defaultDisplay(){
+    let userData = JSON.parse(localStorage.getItem('archive'));
+    if(userData === null){
+        console.log('yo')
+        archive.push( windwaker = {
+            title: 'The Legend of Zela: The Wind Waker',
+            genre: 'Adventure/Puzzle',
+            hours: 150,
+            completed: 'completed',
+            image: ww
+        },
+        dragon = {
+            title: 'Yakuza: Like a Dragon',
+            genre: 'JRPG',
+            hours: 70,
+            completed: 'not completed',
+            image: ykl
+        },
+        yakuza = {
+            title: 'Yakuza Kiwami 2 (Sub)',
+            genre: 'Action/Adventure',
+            hours: 70,
+            completed: 'completed',
+            image: yk
+        },
+        SA2 = {
+            title: 'Sonic Adventure 2 Battle',
+            genre: 'Action/Adventure',
+            hours: 1000,
+            completed: 'completed',
+            image: sonic
+        },
+    
+        ninja = {
+            title: 'Ninja Gaiden Black',
+            genre: 'Action/Hard...',
+            hours: 50,
+            completed: 'completed',
+            image: gaiden
+        })
+
+        localStorage.setItem('archive', JSON.stringify(archive));
+        displayArchive()
+    } 
+    else {
+        displayArchive()
+    }
+}
+
+defaultDisplay()
+
+// displayArchive()
